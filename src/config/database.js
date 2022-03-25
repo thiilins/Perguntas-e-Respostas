9f6da7b345c9c3525b78584b9a96300f7c4d871f
Thiago Lins <taglins@gmail.com>
2022-03-25 00:29:45 -0300

    [Update] Configurações Sequelize

--- src/config/database.js
+++ src/config/database.js
@@ -1,23 +1,24 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+"use strict";
+module.exports = {
+  development: {
+    username: "root",
+    password: null,
+    database: "questions_and_answers",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "questions_and_answers",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: "root",
+    password: null,
+    database: "questions_and_answers",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
