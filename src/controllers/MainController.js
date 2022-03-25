fefac11bd2487f89b1630b77c8a769db3178e480
Thiago Lins <taglins@gmail.com>
2022-03-25 00:23:20 -0300

    [Update] Alterando o idioma do código para EN

--- src/controllers/MainController.js
+++ src/controllers/MainController.js
@@ -3,10 +3,10 @@ const MainController = {
   index(req, res) {
     res.render("index");
   },
-  viewPerguntar(req, res) {
-    res.render("perguntar", { title: "Realizar Perguntas" });
+  askView(req, res) {
+    res.render("ask", { title: "Realizar Perguntas" });
   },
-  perguntar(req, res) {
+  ask(req, res) {
     const { title, description } = req.body;
     console.log(title, description);
     res.send("Formulario Enviado com Sucesso!");
--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -3,6 +3,6 @@ const express = require("express");
 const router = express.Router();
 const MainController = require("../controllers/MainController");
 router.get("/", MainController.index);
-router.get("/perguntar", MainController.viewPerguntar);
-router.post("/perguntar", MainController.perguntar);
+router.get("/ask-a-question", MainController.askView);
+router.post("/ask-a-question", MainController.ask);
 module.exports = router;
