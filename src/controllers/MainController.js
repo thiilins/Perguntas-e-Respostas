5fe95847bf3ed04efd68b5cb84c1ad31eab997af
Thiago Lins <taglins@gmail.com>
2022-03-24 23:39:59 -0300

    [Add] Rotas: Perguntar (POST/GET)

--- src/controllers/MainController.js
+++ src/controllers/MainController.js
@@ -3,5 +3,13 @@ const MainController = {
   index(req, res) {
     res.render("index");
   },
+  viewPerguntar(req, res) {
+    res.render("perguntar", { title: "Realizar Perguntas" });
+  },
+  perguntar(req, res) {
+    const { title, description } = req.body;
+    console.log(title, description);
+    res.send("Formulario Enviado com Sucesso!");
+  },
 };
 module.exports = MainController;
--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -3,4 +3,6 @@ const express = require("express");
 const router = express.Router();
 const MainController = require("../controllers/MainController");
 router.get("/", MainController.index);
+router.get("/perguntar", MainController.viewPerguntar);
+router.post("/perguntar", MainController.perguntar);
 module.exports = router;
