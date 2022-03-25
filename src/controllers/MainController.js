3ebe937b98899e9a8806f69f9618cdbdf4e302a8
Thiago Lins <taglins@gmail.com>
2022-03-25 02:41:57 -0300

    [Update] Sequelize Config

--- src/controllers/MainController.js
+++ src/controllers/MainController.js
@@ -1,15 +1,22 @@
 "use strict";
+const { Question } = require("../models");
 const MainController = {
-  index(req, res) {
-    res.render("index");
+  async index(req, res) {
+    const questionList = await Question.findAll();
+    console.log(questionList);
+    res.render("index", { questionList });
   },
   askView(req, res) {
     res.render("ask", { title: "Realizar Perguntas" });
   },
-  ask(req, res) {
+  async ask(req, res) {
     const { title, description } = req.body;
-    console.log(title, description);
-    res.send("Formulario Enviado com Sucesso!");
+    await Question.create({
+      title: title,
+      description: description,
+    });
+    res.redirect("/");
   },
+  async listQuestions(req, res) {},
 };
 module.exports = MainController;
